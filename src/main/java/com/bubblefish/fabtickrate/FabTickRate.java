4a6b162f30b67d3264e30b6b1f0171e94c90e72d
Force <daforsastudia@gmail.com>
2020-10-12 13:28:10 +0300

    0.0.1 Ready to work workspace

---
+++ src/main/java/com/bubblefish/fabtickrate/FabTickRate.java
@@ -0,0 +1,10 @@
+package com.bubblefish.fabtickrate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabTickRate implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
